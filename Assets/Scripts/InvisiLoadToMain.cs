7ef762bbd817b5db5bde9d76fb3f66ec20c51f9a
Omari Straker <straka3000@gmail.com>
2019-03-17 17:25:44 -0700

    Add Parallax BG to every scene and adjust levls accordingly. Add invisible barrier to end level to go back to main menu

---
+++ Assets/Scripts/InvisiLoadToMain.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InvisiLoadToMain : MonoBehaviour {
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag.Equals("Player"))
+        {
+            GoBackToMain();
+        }
+    }
+
+    /// <summary>
+    /// Also called by Button Click
+    /// </summary>
+    public void GoBackToMain()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
